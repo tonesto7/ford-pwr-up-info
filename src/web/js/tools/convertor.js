a2385d442f752985a6edeb8d31fb90f04ef17c9c
Anthony S <tonesto7@gmail.com>
2023-04-04 18:28:15 -0400

    syncing mach-e data

--- src/web/js/tools/convertor.js
+++ src/web/js/tools/convertor.js
@@ -1,4 +1,7 @@
-const chgs = ["Minor adjustments[41]", "Some camera views are now available while the vehicle is in motion[42]", "Added Picture-in-Picture capability to the Trailer Reverse Guidance feature", "Updated SYNC to Software Version: 22034_PRODUCT Revision: 364(2.5.3)[43]", "Updated SYNC to Software Version: 22095_PRODUCT Revision: 421(2.8.3)"];
+const chgs = [
+    "Adds ability to enter your vehicle using your Key Fob during a non-drivable update",
+    "You'll now receive new pop-up alerts to inform you when a precondition is not being met so that you can take the required action to receive your update. For a non-drivable update, preconditions may include that your vehicle is parked, it's not running, all the lights are off, doors and trunk are closed, and you're not pressing the brake pedal.[13]",
+];
 
 let chgsOut = [];
 for (let [i, chg] of chgs.entries()) {
